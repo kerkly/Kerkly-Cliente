4793b4306b6dbc853eab1eb8571361d23beacfe5
JosMan9 <josem_rl@hotmail.com>
2022-06-12 16:04:57 -0500

    Boton de No registrado segun el device ID

--- app/src/main/java/com/example/kerklyv5/url/Url.kt
+++ app/src/main/java/com/example/kerklyv5/url/Url.kt
@@ -2,5 +2,5 @@ package com.example.kerklyv5.url
 
 class Url {
    // val url: String = "https://www.saua.uagro.mx/cliente"
-   val url: String = "https://2163-189-143-69-188.ngrok.io/Kerkly/cliente"
+   val url: String = "https://d426-2806-104e-3-c454-3d23-58b4-92e-e660.ngrok.io/Kerkly/cliente"
 }
