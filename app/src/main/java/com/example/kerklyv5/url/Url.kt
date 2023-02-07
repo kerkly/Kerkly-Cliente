66661f8799f101a1101d694048135603194d85f6
RedWifiNet <99827320+Luis0599@users.noreply.github.com>
2023-02-07 16:46:05 -0600

    cliente 07 de febrero2

--- app/src/main/java/com/example/kerklyv5/url/Url.kt
+++ app/src/main/java/com/example/kerklyv5/url/Url.kt
@@ -3,6 +3,7 @@ package com.example.kerklyv5.url
 class Url {
 
    //val url: String = "https://www.saua.uagro.mx/cliente"
+    //pruebagh
  val url: String = "https://c19b-189-143-80-161.ngrok.io/kerkly/cliente"
 
 }
